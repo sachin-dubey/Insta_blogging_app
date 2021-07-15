467a2d793664435a782043ef4d35758effab7e2e
sachin_d <sachindubey1998@gmail.com>
2021-07-15 15:41:29 +0530

    Insta_blogging_app

---
+++ app/src/main/java/com/sachin_dubey/instag/Model/Comments.java
@@ -0,0 +1,13 @@
+package com.sachin_dubey.instag.Model;
+
+public class Comments extends CommentsId{
+      private String comment , user;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getUser() {
+        return user;
+    }
+}
---
+++ app/src/main/java/com/sachin_dubey/instag/Model/Users.java
@@ -0,0 +1,13 @@
+package com.sachin_dubey.instag.Model;
+
+public class Users {
+    private String image , name;
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
